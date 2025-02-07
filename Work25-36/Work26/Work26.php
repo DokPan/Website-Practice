8a327a8b70900cacb6a18a4fa767611737c30aaf
Alina <alinaroshchina5@gmail.com>
2025-02-07 19:00:12 +0300

    Create Work26.php

---
+++ Work25-36/Work26/Work26.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+ <title>Test PHP</title>
+ </head>
+ <body>
+ <?php $a=5 ?>
+ <?php $b=6 ?>
+ <?php if ($a+$b == 10): ?>
+ True
+<?php else: ?>
+ <?php 
+ echo $a,$b
+ ?>
+<?php endif; ?>
+ </body>
+</html>
