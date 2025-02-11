2ef6924c89ab620a63da4e94366c1b5b3b2a2df6
Alina <alinaroshchina5@gmail.com>
2025-02-11 12:47:19 +0300

    Create Work29.php

---
+++ Work25-36/Work29/Work29.php
@@ -0,0 +1,15 @@
+<?php
+include 'root.php';
+$x = 4; $n = 2;
+echo calculateRoot($x, $n);
+
+include 'function.php';
+$a = 4;
+$x = -2;
+echo calculateY($a, $x);
+
+include 'leap_year.php';
+$month = 2; $year = 2024;
+echo "Year $year " . (isLeapYear($year) ? "visokosnii" : "not visokosnii") . "<br>";
+echo "Days in month: " . daysInMonth($month, $year) . "<br>";
+?>
